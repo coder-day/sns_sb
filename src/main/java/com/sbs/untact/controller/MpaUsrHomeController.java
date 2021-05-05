f7b4fe442e0ce3e7615c3ad55280db4bcfc7294c
coder-day <nykim@gmail.com>
2021-05-05 23:20:31 +0900

    컨트롤러 테스트

---
+++ src/main/java/com/sbs/untact/controller/MpaUsrHomeController.java
@@ -0,0 +1,67 @@
+package com.sbs.untact.controller;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+public class MpaUsrHomeController {
+	@RequestMapping("mpaUsr/home/main")
+	@ResponseBody
+	public String showmain() {
+		return "안녕";
+	}
+	
+	@RequestMapping("mpaUsr/home/main2")
+	@ResponseBody
+	public String showmain2() {
+		return "잘가";
+	}
+	
+	@RequestMapping("mpaUsr/home/main3")
+	@ResponseBody
+	public int showmain3(int a, int b) {
+		return a + b;
+	}
+
+	@RequestMapping("mpaUsr/home/main4")
+	@ResponseBody
+	public int showmain4(int a, int b) {
+		return a - b;
+	}
+
+	@RequestMapping("mpaUsr/home/main5")
+	@ResponseBody
+	public String showmain5() {
+		return "-20";
+	}
+	
+	@RequestMapping("mpaUsr/home/main6")
+	@ResponseBody
+	public boolean showmain6() {
+		return 1 > 2;
+	}
+	
+	@RequestMapping("mpaUsr/home/main7")
+	@ResponseBody
+	public int[] showmain7() {
+		int[] arr = new int[] {1,2,3};
+		return arr;
+	}
+	
+	@RequestMapping("mpaUsr/home/main8")
+	@ResponseBody
+	public List<Integer> showmain8() {
+		List<Integer> l = new ArrayList<>();
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		return l;
+	}
+	
+}
