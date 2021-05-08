0240ef68e72e1f0325695e83a4971b467b56ec0b
coder-day <nykim@gmail.com>
2021-05-08 19:47:57 +0900

    게시물수정

--- src/main/java/com/sbs/untact/controller/MpaUsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/MpaUsrArticleController.java
@@ -17,15 +17,21 @@ import com.sbs.untact.util.Util;
 
 @Controller
 public class MpaUsrArticleController {
+	//게시물 저장 배열
 	private List<Article> articles;
+	// 글번호 전역변수
 	private int articleLastId;
 	
 	//초기화 
 	public MpaUsrArticleController() {
 		articles = new ArrayList<>();
 		articleLastId = 0;
+		//글 자동 생성
+		makeTestData();
 	}
 
+	
+	// 게시물 쓰기
 	@RequestMapping("/mpaUsr/article/doWrite")
 	@ResponseBody
 	public ResultData doWrite(String title, String body) {
@@ -34,20 +40,36 @@ public class MpaUsrArticleController {
 		Article article = getArticleById(id);
 		return new ResultData("S-1", id + "번 글이 작성되었습니다.", "article", article);
 	}
-
-	private int writeArticle(String title, String body) {
-		
-		int id = articleLastId + 1;
-		String regDate = Util.getNowDateStr();
-		String updateDate = Util.getNowDateStr();
-		
-		Article article = new Article(id, regDate, updateDate, title, body);
-		articles.add(article);
-		articleLastId = id;
-		
-		return id;
+	
+	
+	// 게시물 수정
+	@RequestMapping("/mpaUsr/article/doModify")
+	@ResponseBody
+	public ResultData doModify(int id, String title, String body) {
+		boolean modified = modifyArticle(id,title, body);
+		if ( modified  == false ) {
+			return new ResultData("F-1", id + "번 글이 없습니다.", "id", id);
+		}
+		return new ResultData("S-1", id + "번 글이 수정 되었습니다.","article", getArticleById(id));	
 	}
 
+	// 게시물 삭제
+	@RequestMapping("/mpaUsr/article/doDelete")
+	@ResponseBody
+	public ResultData doDelete(int id) {
+		boolean deleted = deleteArticleById(id);
+
+		if ( deleted  == false ) {
+			return new ResultData("F-1", id + "번 글이 없습니다.", "id", id);
+		}
+		return new ResultData("S-1", id + "번 글이 삭제 되었습니다.","id", id);
+	}
+
+	
+
+
+
+	// GET 게시물 호출
 	@RequestMapping("/mpaUsr/article/getArticle")
 	@ResponseBody
 	public ResultData getArticle(int id) {
@@ -60,14 +82,18 @@ public class MpaUsrArticleController {
 		return new ResultData("S-1", article.getId() + "번 글입니다.", "article", article);
 	}
 
+	
+	// 게시물 GET ID로 호출 함수
+	
 	private Article getArticleById(int id) {
 		
-		//리스트 반복
+		//리스트 반복-> 향상된 for문
 		for ( Article article : articles ) {
 			if (article.getId() == id ) {
 				return article;
 			}
 		}
+		//리스트 반복 정석
 //		for ( int i = 0; i < articles.size(); i++) {
 //			Article article = articles.get(i);
 //			
@@ -77,6 +103,66 @@ public class MpaUsrArticleController {
 //		}
 		
 		return null;
+	}
+	// 시작시 자동 글 등록(테스트)
+	private void makeTestData() {
+		for (int i = 0; i < 10; i++) {
+			writeArticle("제목1","내용1");
+		}
+	}
+	
+
+	//게시물 수정 함수
+	private boolean modifyArticle(int id, String title, String body) {
+		Article article = getArticleById(id);
+		
+		if ( article == null) {
+			return false;
+		}
+		
+		article.setUpdateDate(Util.getNowDateStr());
+		article.setTitle(title);
+		article.setBody(body);
+		
+		return true;
+	}
+
+	
+	
+	// 게시물 쓰기 함수
+	private int writeArticle(String title, String body) {
+		
+		int id = articleLastId + 1;
+		String regDate = Util.getNowDateStr();
+		String updateDate = Util.getNowDateStr();
+		
+		Article article = new Article(id, regDate, updateDate, title, body);
+		articles.add(article);
+		articleLastId = id;
+		
+		return id;
+	}
+	
+	// 게시물 삭제 함수
+	private boolean deleteArticleById(int id) {
+		
+		Article article = getArticleById(id);
+		
+		if ( article == null) {
+			return false;
+		}
+		
+		articles.remove(article);
+		return true;
+		// articles.remove(1) --> 2번째 글 삭제
+//		for ( Article article : articles ) {
+//			if (article.getId() == id ) {
+//				articles.remove(article);
+//				return true;
+//			}
+//		}
+//		
+//		return false;
 	}
 }
 
