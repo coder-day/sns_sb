054cb7a88b176963d0f36e01156ef1ed058c1f7d
coder-day <nykim@gmail.com>
2021-05-11 11:32:14 +0900

    lastInsertID

--- src/main/java/com/sbs/untact/dao/ArticleDao.java
+++ src/main/java/com/sbs/untact/dao/ArticleDao.java
@@ -10,5 +10,6 @@ public interface ArticleDao {
 	boolean modifyArticle(@Param("id") int id, @Param("title")String title, @Param("body")String body);
 	void writeArticle(@Param("boardId") int boardId, @Param("memberId") int memberId, @Param("title") String title, @Param("body") String body);
 	Article getArticleById(@Param("id") int id);
+	int getLastInsertId();
 	void deleteArticleById(@Param("id") int id);
 }
