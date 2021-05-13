bc0daf2c7c5c95dd59aa8a22c3d2653fccf026fb
coder-day <nykim@gmail.com>
2021-05-13 09:44:35 +0900

    공통 에러 페이지추가

--- src/main/java/com/sbs/untact/controller/MpaUsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/MpaUsrArticleController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,53 +16,37 @@ import com.sbs.untact.util.Util;
 
 @Controller
 public class MpaUsrArticleController {
-	
-
 	@Autowired
 	private ArticleService articleService;
-
 	@RequestMapping("/mpaUsr/article/doWrite")
 	@ResponseBody
 	public ResultData doWrite(String title, String body) {
-		
-	
 		if (Util.isEmpty(title)) {
 			return new ResultData("F-1", "제목을 입력해주세요.");
 		}
-		
-	
 		if (Util.isEmpty(body)) {
 			return new ResultData("F-2", "내용을 입력해주세요.");
 		}
-
-			return articleService.writeArticle(title, body);
+		return articleService.writeArticle(title, body);
 	}
-
 	@RequestMapping("/mpaUsr/article/doModify")
 	@ResponseBody
 	public ResultData doModify(Integer id, String title, String body) {
-		
 		if (Util.isEmpty(id)) {
 			return new ResultData("F-1", "번호를 입력해주세요.");
 		}
-		
 		if (Util.isEmpty(title)) {
 			return new ResultData("F-2", "제목을 입력해주세요.");
 		}
-		
 		if (Util.isEmpty(body)) {
 			return new ResultData("F-3", "내용을 입력해주세요.");
 		}
-
 		Article article = articleService.getArticleById(id);
-
 		if (article == null) {
 			return new ResultData("F-4", "존재하지 않는 게시물 번호입니다.");
 		}
-
 		return articleService.modifyArticle(id, title, body);
 	}
-
 	@RequestMapping("/mpaUsr/article/doDelete")
 	@ResponseBody
 	public ResultData doDelete(Integer id) {
@@ -72,38 +55,28 @@ public class MpaUsrArticleController {
 		}
 		return articleService.deleteArticleById(id);
 	}
+	@RequestMapping("/mpaUsr/article/list")
+	public String showList(HttpServletRequest req, int boardId) {
+		Board board = articleService.getBoardById(boardId);
 
+		if (board == null) {
+			req.setAttribute("msg", boardId + "번 게시판이 존재 하지 않습니다.");
+			return "common/redirect";
+		}
+
+		req.setAttribute("board", board);
+		return "mpaUsr/article/list";
+	}
 	@RequestMapping("/mpaUsr/article/getArticle")
 	@ResponseBody
 	public ResultData getArticle(Integer id) {
-
 		if (Util.isEmpty(id)) {
 			return new ResultData("F-1", "번호를 입력해주세요.");
 		}
-		
-
 		Article article = articleService.getArticleById(id);
-
 		if (article == null) {
-
 			return new ResultData("F-1", id + "번 글은 존재하지 않습니다.", "id", id);
 		}
 		return new ResultData("S-1", article.getId() + "번 글 입니다.", "article", article);
-	}
-	
-	@RequestMapping("/mpaUsr/article/list")
-	@ResponseBody
-	public String showList(HttpServletRequest req,int boardId) {
-		
-		Board board = articleService.getBoardById(boardId);
-		
-		if ( board == null ) {
-			return "존재 하지 않은 게시판 입니다.";
-		}
-		
-		req.setAttribute("board", board);
-		
-		return "mpaUsr/article/list";
-	
 	}
 }
